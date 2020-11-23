4cc448139c5aa03433d07e3d01177ff7aed0152e
mtrimner <matt.trimner@gmail.com>
2020-11-23 11:40:45 -0800

    commit tree 1899341cceaf0b4d5c102a00e650fb8876c0e065

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+fetch(imgUrl)
+.then(function(response){
+    return response.json()
+}).then(function(images){
+    images.message.forEach(function(image){
+        console.log(image)
+    })
+    })
